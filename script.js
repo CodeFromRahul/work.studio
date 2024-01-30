f994a9df12b899329eeae1ed4e1640688ff41422
CodeFromRahul <rahulvish194002@gmail.com>
2024-01-30 18:35:44 +0530

    Work Website Clone

---
+++ script.js
@@ -0,0 +1,57 @@
+function loading(){
+
+var tl = gsap.timeline();
+
+tl.to("#yellow1",{
+top :"-100%",
+delay:0.5,
+duration:0.5,
+ease:"expo.out"
+})
+ 
+tl.from("#yellow2",{
+    top :"100%",
+delay:0.5,
+duration:0.5,
+ease:"expo.out"
+},"am")
+
+tl.to("#loader h1",{
+    duration:0.5,
+    delay:0.2,
+    color:"black"
+},"am")
+tl.to("#loader ",{
+   display:"none"
+})
+tl.to("#loader ",{
+    opacity:0
+})
+
+
+}
+
+loading();
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+var elems = document.querySelectorAll(".elem")
+
+var page2 = document.querySelector("#page2")
+
+elems.forEach(function(ele){
+    ele.addEventListener("mouseenter",function(){
+        var bgimg = ele.getAttribute("data-img")
+        page2.style.backgroundImage =`url(${bgimg})`
+    })
+
+})
+
+
+
+
+
